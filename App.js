a59e481894ec3de447b3332e86390e490593c472
Tới Phạm <44054808+tom1097@users.noreply.github.com>
2019-10-22 17:34:43 +0700

    #1

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, ScrollView, TextInput, Button} from 'react-native';
+import {Avatar, Icon} from 'react-native-elements';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={{flex: 8}}>
+                <ScrollView>
+                    <View style={styles.withicon}>
+                        <Avatar rounded size={'medium'} title="TP"/>
+                        <Text style={styles.msgbox}>Tada</Text>
+                    </View>
+                    <View style={styles.withicon}>
+                        <Avatar rounded size={'medium'} title="TP"/>
+                        <Text style={styles.msgbox}>Open up App.js to start working on your app!</Text>
+                    </View>
+
+                </ScrollView>
+            </View>
+            <View style={styles.blockaction}>
+                <View style={styles.inputmessage}>
+                    <TextInput
+                        style={{height: 40}}
+                        placeholder="Hey!"
+                    />
+                </View>
+                <View style={styles.btnsend}>
+                    <Button title={'Send'} style={styles.btnsend}></Button>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        padding: 10,
+        flex: 1,
+    },
+    inputmessage: {
+        width: '80%',
+    },
+    btnsend: {
+        width: '20%',
+    },
+    msgbox: {
+        backgroundColor: '#f1f0f0',
+        borderRadius: 4,
+        borderWidth: 1,
+        borderColor: '#d6d7da',
+        fontSize: 17,
+        padding: 10,
+        width: '90%',
+    },
+    blockaction: {
+        flex: 1,
+        flexDirection: "row",
+        padding: 5,
+        backgroundColor: '#f1f0f0',
+        justifyContent: 'center',
+    },
+    withicon: {
+        flexDirection: "row",
+    },
 });
